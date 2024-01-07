517287cf47d632cd744faacd98205445fe8b1eda
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-01-07 16:56:54 +0100

    Add scrollsnap

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', start);
 
+var parallaxEnable = true;
+
 function start() {
     var centerPos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
     var planets = document.getElementsByClassName('planet');
     var sun = document.getElementsByClassName('sun');
+    var slider = document.getElementsByClassName('slider');
+
     document.addEventListener('mousemove', function (e) {
         var baseSpeed = 0.5;
         var mousePos = {
@@ -14,21 +18,46 @@ function start() {
         applyEffect(planets, mousePos, baseSpeed);
         applyEffect(sun, mousePos, baseSpeed);
     });
+
+    slider[0].addEventListener('scroll', function (e) {
+        if (slider[0].scrollTop == 0) {
+            parallaxEnable = true;
+        } else {
+            parallaxEnable = false;
+        }
+    });
+
+    var sections = slider[0].children;
+    var observer = new IntersectionObserver(function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > 0) {
+                if (entry.isIntersecting && entry.target.id != 'system') {
+                    var item = entry.target.id + 'Img';
+                    var planet = document.getElementById(item);
+
+                    gsap.fromTo(planet, { x: 1000 }, { x: 0, duration: 1 });
+                }
+            }
+        });
+    }, { threshold: 0.5 });
+
+    for (var i = 0; i < sections.length; i++) {
+        observer.observe(sections[i]);
+    }
 }
 
 function applyEffect(planets, mousePos, baseSpeed) {
-    Array.from(planets).forEach(planet => {
+    if (parallaxEnable) {
+        Array.from(planets).forEach(planet => {
+            var speedX = parseFloat(planet.dataset.speedx) || 1;
+            var speedY = parseFloat(planet.dataset.speedy) || 1;
 
-        var speedX = parseFloat(planet.dataset.speedx) || 1;
-        var speedY = parseFloat(planet.dataset.speedy) || 1;
-        
-        
-        speedX *= baseSpeed;
-        speedY *= baseSpeed;
-        
+            speedX *= baseSpeed;
+            speedY *= baseSpeed;
 
-        moveObject(planet, mousePos, speedX, speedY);
-    });
+            moveObject(planet, mousePos, speedX, speedY);
+        });
+    }
 }
 
 function moveObject(planet, mousePos, speedX, speedY) {
